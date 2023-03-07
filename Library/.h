fb57e772157416ed7cfeec50040db50eae96c8d2
Askhab Aslakhoy <115141200+HadjiMurad1852@users.noreply.github.com>
2023-03-07 13:56:58 +0300

    Create .h

---
+++ Library/.h
@@ -0,0 +1,12 @@
+#pragma once
+#include <iostream>
+
+using namespace std;
+
+namespace header {
+	string A = "AAA";
+	string B = "BBB";
+	string C = "CCC";
+	string D = "DDD";
+	string E = "EEE";
+}
